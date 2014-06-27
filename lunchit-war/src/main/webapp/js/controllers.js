2bab0cd9cd9c238d97f38acae5b058dffe8a8918
Zhi Chen <cz.michael@gmail.com>
2014-06-26 22:23:02 -0500

    add angular controller

--- lunchit-war/src/main/webapp/js/controllers.js
+++ lunchit-war/src/main/webapp/js/controllers.js
@@ -2,11 +2,13 @@ var lunchItApp = angular.module('lunchItApp', []);
 
 lunchItApp.controller('MenuListCtrl', function ($scope) {
   $scope.menus = [
-    {'menuName': 'Dou Chi Chicken',
-     'menuDetail': 'Chicken steamed with black beans'},
-    {'menuName': 'Ma po Tofu',
-     'menuDetail': 'Spicy Tofu'},
-    {'menuName': 'Shu Zhu Yu',
-     'menuDetail': 'Fish in a spicy soup'}
+    {'menuName': '豆豉鸡',
+     'price': '6.50'},
+    {'menuName': '麻婆豆腐',
+     'price': '7.00'},
+    {'menuName': '香菇时菜',
+     'price': '6.00'},
+    {'menuName': '水煮鱼',
+     'price': '8.00'}
   ];
 });
