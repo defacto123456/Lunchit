09c6a6cf23ae04c6017d41cac0adea02eb50b177
Ziyang <defacto123456@gmail.com>
2014-06-29 16:44:12 -0500

    Update user resource

--- lunchit-war/src/main/java/com/czl/li/services/implementation/UserServiceImpl.java
+++ lunchit-war/src/main/java/com/czl/li/services/implementation/UserServiceImpl.java
@@ -1,16 +1,31 @@
 package com.czl.li.services.implementation;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import com.czl.li.dao.UserDao;
+import com.czl.li.data.model.User;
 import com.czl.li.services.UserService;
 
 @Service("userService")
 public class UserServiceImpl implements UserService {
+	@Autowired
+	@Qualifier("userDao")
+	private UserDao userDao;
 
 	@Override
-	public void verifyUserEmail(final String email) {
-		// TODO Auto-generated method stub
+	public void verifyUserEmail(final String email, final String firstName,
+			final String lastName) {
 
+		if (!userDao.isUserExisted(email)) {
+			// create new user
+			final User user = new User();
+			user.setFbEmail(email);
+			user.setFirstName(firstName);
+			user.setLastName(lastName);
+			userDao.createUser(user);
+		}
 	}
 
 }
